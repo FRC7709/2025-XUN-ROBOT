162bf4a9132978a6f3704cbbb9faf3fd76964860
Justin10612 <atom.9031@gmail.com>
2025-03-29 17:49:23 +0800

    Add position and velocity

--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -35,9 +35,12 @@ public class ElevatorSubsystem extends SubsystemBase {
   private boolean ifChange_Low;
 
   public ElevatorSubsystem() {
+    // Motors
     elevator_FirstMotor = new TalonFX(ElevatorConstants.elevator_FirstMotor_ID);
     elevator_SecondMotor = new TalonFX(ElevatorConstants.elevator_SecondMotor_ID);
+    // Motor Followers
     elevator_SecondMotor.setControl(new Follower(ElevatorConstants.elevator_FirstMotor_ID, true));
+    // Set the goal position to the primitive position
     goalPosition = ElevatorConstants.primitivePosition;
 
     // Motor Configurations
@@ -89,55 +92,54 @@ public class ElevatorSubsystem extends SubsystemBase {
     goalPosition = ElevatorConstants.algaeProccesorPosition;
   }
 
-  public void intakeAlgae_Low() {
-    goalPosition = ElevatorConstants.algaeL2Position;
-  }
+  public void intakeAlgae_Floor() {goalPosition = ElevatorConstants.algaeFloorPosition;}
+  public void intakeAlgae_Low() {goalPosition = ElevatorConstants.algaeL2Position;}
+  public void intakeAlgae_High() {goalPosition = ElevatorConstants.algaeL3Position;}
 
-  public void intakeAlgae_High() {
-    goalPosition = ElevatorConstants.algaeL3Position;
-  }
-
-  public void intakeAlgae_Floor() {
-    goalPosition = ElevatorConstants.algaeFloorPosition;
-  }
-
-  public void toPrimitive() {
-    goalPosition = ElevatorConstants.primitivePosition;
-  }
+  public void toPrimitive() {goalPosition = ElevatorConstants.primitivePosition;}
 
   public void prepareForScore() {
     goalPosition = ElevatorConstants.prepareForScorePosition;
   }
 
-  public double getCurrentPosition() {
+  public double getPositionRot() {
     return elevator_FirstMotor.getPosition().getValueAsDouble();
   }
 
-  public boolean arriveSetPoint() {
-    return (Math.abs(goalPosition - getCurrentPosition()) <= 2);
+  public double getPositionMeter(){
+    return elevator_FirstMotor.getPosition().getValueAsDouble()*0.02872; // Meter
+  }
+
+  public double getVelocity() {
+    return elevator_FirstMotor.getVelocity().getValueAsDouble()*0.02872; // Meter per second
+  }
+
+  public boolean arriveSetpoint() {
+    return (Math.abs(goalPosition - getPositionRot()) <= 2);
   }
 
   public boolean arrivePrimition() {
-    return (Math.abs(ElevatorConstants.primitivePosition - getCurrentPosition()) <= 1);
+    return (Math.abs(ElevatorConstants.primitivePosition - getPositionRot()) <= 1);
   }
 
-  public double getGoalPosition() {
+  public double getSetpoint() {
     return goalPosition;
   }
 
   @Override
   public void periodic() {
+    // Elevator control loop
     elevator_FirstMotor.setControl(request_Elevator.withPosition(goalPosition));
 
-    if (getCurrentPosition() <= 30) {
+    if (getPositionRot() <= 30) {
       ElevatorConstants.arriveLow = true;
     }else{
       ElevatorConstants.arriveLow = false;
     }
 
-    if ((getCurrentPosition() < 8 && lastPosition > 8)) {
+    if ((getPositionRot() < 8 && lastPosition > 8)) {
       ifChange_Low = true;
-    }else if((getCurrentPosition() > 8 && lastPosition < 8)){
+    }else if((getPositionRot() > 8 && lastPosition < 8)){
       ifChange_High = true;
     }
 
@@ -170,11 +172,13 @@ public class ElevatorSubsystem extends SubsystemBase {
       System.out.println("sethigh");
     }
 
-    lastPosition = getCurrentPosition();
+    lastPosition = getPositionRot();
 
-    //SmartDashboard
-    SmartDashboard.putNumber("Elevator/GoalPosition", goalPosition);
-    SmartDashboard.putNumber("Elevator/CurrentPosition", getCurrentPosition());
-    SmartDashboard.putBoolean("Elevator/ArrivePosition", arriveSetPoint());
+    // Log
+    SmartDashboard.putNumber("Elevator/GoalPositionRot", goalPosition);
+    SmartDashboard.putNumber("Elevator/CurrentPositionRot", getPositionRot());
+    SmartDashboard.putBoolean("Elevator/ArriveSetpoint", arriveSetpoint());
+    SmartDashboard.putNumber("Elevator/VelocityMeter", getVelocity()); // Meter per second
+    SmartDashboard.putNumber("Elevator/PositionMeter",  getPositionMeter()); // Meter
   }
 }
